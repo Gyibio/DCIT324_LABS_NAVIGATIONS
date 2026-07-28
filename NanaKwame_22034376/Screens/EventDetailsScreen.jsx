7cc3de4e1db3b58495946b9942b6f61fc8197f6e
Nana Kwame Adom-Boateng <royadom480@gamil.com>
2026-07-28 19:38:49 +0000

    Add EventDetailsScreen

---
+++ NanaKwame_22034376/Screens/EventDetailsScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { styles } from '../styles/commonStyles';
+
+export default function EventDetailsScreen({ route, navigation }) {
+  const { event } = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{event.title}</Text>
+      <Text style={styles.cardSubtitle}>Date: {event.date}</Text>
+      <Text style={styles.bodyText}>{event.description}</Text>
+      <Button title="Back to Feed" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
